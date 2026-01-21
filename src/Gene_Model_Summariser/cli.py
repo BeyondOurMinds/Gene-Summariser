bfaf45a0ba68f460af6bad602386797e5c30d56f
BeyondOurMinds <84035484+BeyondOurMinds@users.noreply.github.com>
2026-01-21 16:21:33 +0000

    Fixed current app() argument output, removed __pycache__/ from git repository and tracking

--- src/Gene_Model_Summariser/cli.py
+++ src/Gene_Model_Summariser/cli.py
@@ -3,6 +3,8 @@ import argparse
 
 def app():
     parser = argparse.ArgumentParser(description="A simple CLI tool.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    args = parser.parse_args()
+    
     print('hello world')
     
-    return parser.parse_args()
+    return 0
