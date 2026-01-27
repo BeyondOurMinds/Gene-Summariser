7ed5625dcfcf6ca5a1c06b5c6c7f1c7f16ce877f
dominict1234567-source <dominict1234567@gmail.com>
2026-01-27 10:14:35 +0000

    updated html_generation.py
    makes sure to check we could find the folder to store the data for pillar 3

--- src/Gene_Model_Summariser/html_generation.py
+++ src/Gene_Model_Summariser/html_generation.py
@@ -1,19 +1,27 @@
 #pillar 3 - HTML Report Generation
 
+from multiprocessing import context
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def render_report(context: dict) -> str:
-    # 1) Find the templates folder (relative to this Python file)
-    templates_folder = Path(__file__).resolve().parent / "templates"
+def generate_html_report(tsv_output: dict) -> str: #tsv_output will be renamed once Pillar 1 tsv_output dict is finished
+    pillar3_folder = Path(__file__).resolve().parent # Get the directory of the current file and set as template folder for Jinja2
 
-    # 2) Create a Jinja environment that knows where templates live
-    env = Environment(loader=FileSystemLoader(str(templates_folder)), autoescape=select_autoescape(["html", "xml"]),)
-    # 3) Load your template file by name
-    template = env.get_template("report_template.html")
+    # Set up Jinja2 environment from the folder
+    env = Environment( 
+        loader=FileSystemLoader(str(pillar3_folder)),
+        autoescape=select_autoescape(["html", "xml"]),
+    )
 
-    # 4) Render the template using your context dictionary
-    html_output = template.render(**context)
+    template = env.get_template("groupB.html.j2") # Load the HTML template file from the pillar3 folder
 
+    # tsv_output will be available in Jinja as {{ data }}
+    html_output = template.render(data=tsv_output)
     return html_output
+
+
+
+
+
+
