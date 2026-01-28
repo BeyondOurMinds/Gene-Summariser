e52663849ce18da5c57d1480e80be663113064dc
dominict1234567-source <dominict1234567@gmail.com>
2026-01-28 14:51:11 +0000

    updating main()
    need to chanfe .run.json_builder - doesnt like the . in the function name

--- src/Gene_Model_Summariser/GroupB_Project5.py
+++ src/Gene_Model_Summariser/GroupB_Project5.py
@@ -18,8 +18,8 @@ from .gff_parser import GFF_Parser
 from .gff_validator import check_db
 from .qc_flags_bed import TranscriptWithFlags, write_qc_bed
 from .build_gff import build_gff
-from .run.json_builder import make_run_json_file
-from .run.json_builder import finalise_run_json_file
+from .run_json_builder import make_run_json_file
+from .run_json_builder import finalise_run_json_file
 
 
 # This is the main function for the Gene Model Summariser. 
