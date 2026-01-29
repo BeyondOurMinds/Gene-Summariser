b2e6b0e47b0c5d1856d8e4fc1a1b88251611335c
BeyondOurMinds <84035484+BeyondOurMinds@users.noreply.github.com>
2026-01-29 21:37:33 +0000

    Basic setup for tests

--- tests/test_qc_check.py
+++ tests/test_qc_check.py
@@ -1,7 +1,7 @@
 import pytest
 import gffutils
 from pathlib import Path
-from Gene_Model_Summariser.fasta_validator import FastaChecker
+from Gene_Model_Summariser.QC_check import QC_flags
 from Gene_Model_Summariser.gff_parser import GFF_Parser
 
 @pytest.fixture
@@ -13,3 +13,16 @@ def gff_db_fixture(tmp_path):
     db_path = tmp_path / "test.db"
     db = gffutils.create_db(str(gff_fixture), dbfn=str(db_path), force=True, keep_order=True)
     return db
+
+
+fastaFile = Path(__file__).parent / "Fixtures" / "ref.fasta"
+
+class TestQCCheck:
+
+    def test_gff_QC(self, gff_db_fixture):
+        """
+        Test the gff_QC method to ensure it generates QC flags correctly.
+        """
+
+        flags = QC_flags(gff_db_fixture).gff_QC()
+
